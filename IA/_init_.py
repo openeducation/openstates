d8aa953b2aeb18f6cc301067491d72331ed34872
Matthew Burket <Matthew@assignitapp.com>
2011-02-02 20:03:55 -0600

    Started Iowa

---
+++ IA/_init_.py
@@ -0,0 +1,11 @@
+metadata = {
+	'state_name': 'Iowa'
+	'legislature_name': 'The Iowa Legislature',
+    'lower_chamber_name': 'House',
+    'upper_chamber_name': 'Senate',
+    'lower_title': 'Representative',
+    'upper_title': 'Senator',
+    'lower_term': 2,
+    'upper_term': 4,
+    
+    'sessions'
